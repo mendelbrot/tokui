aec68b96bf53f3ff0e76ae2fc8ad90416653dd36
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-08-11 15:00:19 -0700

    got the basic shapes

--- src/components/Glyph.js
+++ src/components/Glyph.js
@@ -1,18 +1,17 @@
 
-const width = 300
-const height = 300
+const width = 80
+const height = 80
 const border = "2px solid lightgreen"
-const stroke = "slategrey"
-const fill = "red"
+const fill = "black"
 
-const rectStyle = {"stroke": stroke, "fill": fill}
+const rectStyle = {"fill": fill}
 const svgStyle = {"width": width, "height": height, "border": border}
 
 function Glyph() {
   return (
     <div>
       <svg version="1.1" viewBox="0 0 10 10" style={svgStyle}>
-        <line x1="0" y1="1" x2="10" y2="1" stroke="purple" stroke-width="0.1" />
+        {/* <line x1="0" y1="1" x2="10" y2="1" stroke="purple" stroke-width="0.1" />
         <line x1="0" y1="2" x2="10" y2="2" stroke="lightgreen" stroke-width="0.1" />
         <line x1="0" y1="3" x2="10" y2="3" stroke="purple" stroke-width="0.1" />
         <line x1="0" y1="4" x2="10" y2="4" stroke="lightgreen" stroke-width="0.1" />
@@ -30,28 +29,23 @@ function Glyph() {
         <line x1="6" y1="0" x2="6" y2="10" stroke="lightgreen" stroke-width="0.1" />
         <line x1="7" y1="0" x2="7" y2="10" stroke="purple" stroke-width="0.1" />
         <line x1="8" y1="0" x2="8" y2="10" stroke="lightgreen" stroke-width="0.1" />
-        <line x1="9" y1="0" x2="9" y2="10" stroke="purple" stroke-width="0.1" />
+        <line x1="9" y1="0" x2="9" y2="10" stroke="purple" stroke-width="0.1" /> */}
 
-        <rect x="1.25" y="0.75" width="3.5" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="5.25" y="0.75" width="3.5" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
+        <rect x="0.75" y="0.75" width="8.5" height="0.5" rx="0.25" style={rectStyle}/>
+        <rect x="0.75" y="4.75" width="8.5" height="0.5" rx="0.25" style={rectStyle}/>
+        <rect x="0.75" y="8.75" width="8.5" height="0.5" rx="0.25" style={rectStyle}/>
 
-        <rect x="0.75" y="1.25" width="0.5" height="3.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="4.75" y="1.25" width="0.5" height="3.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="8.75" y="1.25" width="0.5" height="3.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
+        <rect x="0.75" y="0.75" width="0.5" height="8.5" rx="0.25" style={rectStyle}/>
+        <rect x="4.75" y="0.75" width="0.5" height="8.5" rx="0.25" style={rectStyle}/>
+        <rect x="8.75" y="0.75" width="0.5" height="8.5" rx="0.25" style={rectStyle}/>
 
-        <rect x="1.25" y="4.75" width="3.5" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="5.25" y="4.75" width="3.5" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
+        <rect x="2.65" y="2.5" width="4.7" height="0.5" rx="0.25" style={rectStyle}/>
+        <rect x="2.65" y="7" width="4.7" height="0.5" rx="0.25" style={rectStyle}/>
 
-        <rect x="0.75" y="5.25" width="0.5" height="3.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="4.75" y="5.25" width="0.5" height="3.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="8.75" y="5.25" width="0.5" height="3.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-
-        <rect x="1.25" y="8.75" width="3.5" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-        <rect x="5.25" y="8.75" width="3.5" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}/>
-
-        <rect x="0.00" y="4.75" width="4.35" height="0.5" stroke-width="0.1" rx="0.25" style={rectStyle}
-          transform-origin="center" transform="rotate(45)"
-        />
+        <rect x="-0.80" y="4.75" width="6" height="0.5" rx="0.25" transform-origin="center" transform="rotate(45)" style={rectStyle} />
+        <rect x="-0.80" y="4.75" width="6" height="0.5" rx="0.25" transform-origin="center" transform="rotate(135)" style={rectStyle} />
+        <rect x="-0.80" y="4.75" width="6" height="0.5" rx="0.25" transform-origin="center" transform="rotate(-45)" style={rectStyle} />
+        <rect x="-0.80" y="4.75" width="6" height="0.5" rx="0.25" transform-origin="center" transform="rotate(-135) "style={rectStyle} />
       </svg>
     </div>
   );
