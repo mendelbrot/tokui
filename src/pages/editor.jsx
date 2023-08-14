0122c8b17864d2340ef5c34e1c1e389490916c45
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-08-14 11:25:37 -0700

    editor cleaned up open and save

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -1,28 +1,66 @@
-import {useState, useEffect} from 'react'
+import { useState } from 'react'
 import Display from '../components/display'
-import textFile from '../data/words.txt'
+
+async function openAsync(setText, setFile) {
+  try {
+    const [filehandle] = await window.showOpenFilePicker({ multiple: false })
+    const file = await filehandle.getFile()
+    if (file) {
+      setFile(file)
+      let reader = new FileReader()
+      reader.onload = e => {
+        const text = e.target.result
+        console.log(text)
+        setText(text)
+      }
+      reader.onerror = e => alert(e)
+      reader.readAsText(file)
+    }
+  } catch (e) {
+    alert(e)
+  }
+}
+
+async function saveAsync(text, file) {
+  try {
+    const options = {
+      suggestedName: file.name,
+      types: [
+        {
+          description: 'Text Files',
+          accept: {
+            'text/plain': ['.txt'],
+          },
+        },
+      ],
+    };
+    const handle = await window.showSaveFilePicker(options)
+    const writable = await handle.createWritable()
+    await writable.write(text)
+    await writable.close()
+  } catch (e) {
+    alert(e)
+  }
+}
 
 function Editor() {
   const [file, setFile] = useState()
   const [text, setText] = useState('')
 
-  const handleFileChange = e => {
-    if (e.target.files) {
-      const file = e.target.files[0]
-      setFile(file)
-      let reader = new FileReader()
-      reader.onload = (e) => {
-        const text = e.target.result
-        console.log(text)
-        setText(text)
-      }
-      reader.onerror = (e) => alert(e.target.error.name)
-      reader.readAsText(file)
-    }
+  const handleOpen = e => {
+    openAsync(setText, setFile)
+  }
+
+  const handleSave = e => {
+    saveAsync(text, file)
   }
 
   const handleTextChange = e => {
-    setText(e.target.value)
+    try {
+      setText(e.target.value)
+    } catch (e) {
+      alert(e)
+    }
   }
 
   return (
@@ -30,8 +68,8 @@ function Editor() {
       <h1 className='mb-6 text-xl'>Editor</h1>
       <Display text={text} />
       <textarea value={text} onChange={handleTextChange} />
-      <input type="file" onChange={handleFileChange} />
-      
+      <button onClick={handleOpen} >Open</button>
+      <button onClick={handleSave} >Save</button>
     </div>
   )
 }
