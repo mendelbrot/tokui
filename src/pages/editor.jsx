fd01a47688264ec5f9875b897cf4aa8808f054c7
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-08-14 10:33:46 -0700

    editor loads file from filestem and edites with textarea

--- src/pages/editor.jsx
+++ src/pages/editor.jsx
@@ -3,20 +3,35 @@ import Display from '../components/display'
 import textFile from '../data/words.txt'
 
 function Editor() {
-  const [text, setText] = useState('');
+  const [file, setFile] = useState()
+  const [text, setText] = useState('')
 
-  const loadWords = async () => {
-    const response = await fetch(textFile)
-    const textContent = await response.text()
-    setText(textContent)
+  const handleFileChange = e => {
+    if (e.target.files) {
+      const file = e.target.files[0]
+      setFile(file)
+      let reader = new FileReader()
+      reader.onload = (e) => {
+        const text = e.target.result
+        console.log(text)
+        setText(text)
+      }
+      reader.onerror = (e) => alert(e.target.error.name)
+      reader.readAsText(file)
+    }
   }
 
-  useEffect(() => { loadWords() })
+  const handleTextChange = e => {
+    setText(e.target.value)
+  }
 
   return (
     <div>
       <h1 className='mb-6 text-xl'>Editor</h1>
       <Display text={text} />
+      <textarea value={text} onChange={handleTextChange} />
+      <input type="file" onChange={handleFileChange} />
+      
     </div>
   )
 }
