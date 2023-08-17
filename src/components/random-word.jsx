4e8711de574aadb1d4c0ded59eba270826545664
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-08-17 11:32:41 -0700

    styling changes to random button

--- src/components/random-word.jsx
+++ src/components/random-word.jsx
@@ -14,10 +14,10 @@ function RandomWord() {
   return (
     <button 
       onClick={handleRandom}
-      className='w-20 h-20 border-2 rounded-lg hover:bg-yellow-50' 
+      className='w-20 h-20 border-2 rounded-lg bg-opacity-70 bg-white hover:bg-yellow-50 hover:bg-opacity-70' 
     >
       <img 
-        className='opacity-40'
+        className='opacity-30 rounded-lg'
         src={randomIcon} 
         alt='go to random word' 
       />
