777bd7b7ce5d70f6afd34d196abb9142449da5aa
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-08-17 23:19:00 -0700

    fixed tags page logic

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -17,11 +17,13 @@ function Tags() {
   return (
     <div className='prose'>
       {tags && tags.map(t => (
-        <div>
+        <div key={'tag ' + t}>
           <h2>{t}</h2>
           <ul>
-            {words.filter(w => w.tags && w.tags.some(t => tags.includes(t))).map(w => (
-              <li>{w.written} {w.meanings[0]}</li>
+            {words.filter(w => w.tags && w.tags.some(wt => t.includes(wt))).map(w => (
+              <li key={'tag ' + t + ' word ' + w.written}>
+                {w.written} {w.meanings[0]}
+              </li>
             ))}
           </ul>
         </div>
