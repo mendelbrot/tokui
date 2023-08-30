21a8cda0b46708a9d3181af223b73a4b5b39cd2c
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-08-29 23:05:14 -0700

    deleted everything.  clean slate

--- src/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className='prose max-w-prose'>
-      <p>Welcome to the website of the conlang named kui.  There is an editor page for writing in kui, and a glyph page showing all 300 written characters.</p>
-    </div>
-  )
-}
-
-export default Home
--- src/pages/root.jsx
+++
@@ -1,4 +0,0 @@
-import { Outlet } from 'react-router-dom'
-import { withNav } from '../components/nav'
-
-export default withNav(Outlet)
