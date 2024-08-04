a8ea44f7a6d09d67c03134242dccb6aa15d3be19
Gregory Maxedon <gregory.maxedon@gmail.com>
2024-08-03 21:40:07 -0700

    editor plain text area

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -8,6 +8,7 @@ import { IoCodeDownload } from 'react-icons/io5'
 import { HiMagnifyingGlassMinus, HiMagnifyingGlassPlus } from 'react-icons/hi2'
 import { CiSquareMinus, CiSquarePlus } from 'react-icons/ci'
 import { BsFiletypeTxt } from 'react-icons/bs'
+import { BsCopy } from 'react-icons/bs'
 
 const maxScaleValue = 5
 const minScaleValue = 0.5
@@ -45,6 +46,8 @@ function Editor() {
   const [windowDimensions, setWindowDimensions] = React.useState<number[]>([
     0, 0,
   ])
+  const [textMode, setTextMode] = React.useState<boolean>(false)
+  const textareaRef = React.useRef<HTMLTextAreaElement | null>(null)
 
   const { glyphSvg, cursorMap } = draw(text, settingsValue)
   // console.table(cursorMap)
@@ -105,6 +108,10 @@ function Editor() {
     right: () => {},
   }
 
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
+  }
+
   React.useEffect(() => {
     setWindowDimensions([window.innerWidth, window.innerHeight])
 
@@ -119,33 +126,73 @@ function Editor() {
     }
   }, [])
 
+  React.useEffect(() => {
+    if (textMode && textareaRef.current) {
+      textareaRef.current.focus()
+    }
+  }, [textMode])
+
   return (
     <div>
       <div className="flex flex-col h-screen w-[344px] sm:w-[568px] p-[16px]">
-        <div className="flex flex-row mt-2 justify-between">
+        <div className="flex flex-row justify-between">
           <div>
-            <button onClick={settings.decrementScale}>
+            <button
+              onClick={settings.decrementScale}
+              className="active:bg-lime-300 p-1 rounded-lg"
+            >
               <HiMagnifyingGlassMinus size={iconSize} />
             </button>
-            <button onClick={settings.incrementScale}>
+            <button
+              onClick={settings.incrementScale}
+              className="active:bg-lime-300 p-1 rounded-lg"
+            >
               <HiMagnifyingGlassPlus size={iconSize} />
             </button>
           </div>
           <div>
-            <button onClick={settings.decrementLineWrap}>
+            <button
+              onClick={settings.decrementLineWrap}
+              className="active:bg-lime-300 p-1 rounded-lg"
+            >
               <CiSquareMinus size={iconSize} />
             </button>
-            <button onClick={settings.incrementLineWrap}>
+            <button
+              onClick={settings.incrementLineWrap}
+              className="active:bg-lime-300 p-1 rounded-lg"
+            >
               <CiSquarePlus size={iconSize} />
             </button>
           </div>
           <div>
-            <button onClick={() => {}}>
+            <button
+              onClick={() => {
+                setTextMode(!textMode)
+              }}
+              className={
+                textMode ? 'bg-lime-300 p-1 rounded-lg' : 'p-1 rounded-lg'
+              }
+            >
               <BsFiletypeTxt size={iconSize} />
+            </button>
+            <button
+              className="active:bg-lime-300 p-1 rounded-lg"
+              onClick={async () => {
+                try {
+                  await navigator.clipboard.writeText(text)
+                } catch (e) {
+                  alert(e)
+                }
+              }}
+            >
+              <BsCopy size={iconSize} />
             </button>
           </div>
           <div>
-            <button onClick={handleDownloadSvg}>
+            <button
+              onClick={handleDownloadSvg}
+              className="active:bg-lime-300 p-1 rounded-lg"
+            >
               <IoCodeDownload size={iconSize} />
             </button>
           </div>
@@ -155,13 +202,23 @@ function Editor() {
           cursorPosition={cursorPosition}
           moveTo={cursor.moveTo}
         />
-        <div className="flex flex-row items-center">
-          <Keyboard
-            text={text}
-            setText={setText}
-            windowWidth={windowDimensions[0]}
+        <div className={!textMode ? 'hidden' : 'h-[216px]'}>
+          <textarea
+            ref={textareaRef}
+            value={text}
+            onChange={handleTextChange}
+            className="border border-slate-700 rounded-lg p-2 h-[216px] w-[312px] sm:w-[536px]"
           />
         </div>
+        {!textMode && (
+          <div className="flex flex-row items-center">
+            <Keyboard
+              text={text}
+              setText={setText}
+              windowWidth={windowDimensions[0]}
+            />
+          </div>
+        )}
       </div>
     </div>
   )
