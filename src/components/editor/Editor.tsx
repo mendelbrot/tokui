2fa40e2f45118f2c01d07c6869dda987f9d14a67
Gregory Maxedon <gregory.maxedon@gmail.com>
2024-09-30 00:53:51 -0700

    homepage reno and some refactoring

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -9,6 +9,7 @@ import Keyboard from '@/components/editor/Keyboard'
 import Display from './Display'
 import SettingsBar from './SettingsBar'
 import { EditorModelProjection } from '@/lib/EditorTypes'
+import useWindowDimensions from '@/lib/useWindowDimensions'
 
 const screens = { sm: 640 }
 
@@ -27,18 +28,15 @@ function Editor() {
 
   const { settings, cursor, writing } = editor.current
 
-  const [windowDimensions, setWindowDimensions] = React.useState<number[]>([
-    0, 0,
-  ])
+  const windowDimensions = useWindowDimensions()
+  const smallScreen = windowDimensions[0] < screens.sm
   const [displayHeight, setDisplayHeight] = React.useState<number | undefined>(
     undefined
   )
   const [textMode, setTextMode] = React.useState<boolean>(false)
-  const [gridMode, setGridMode] = React.useState<boolean>(true)
+  const [gridMode, _setGridMode] = React.useState<boolean>(true)
   const textareaRef = React.useRef<HTMLTextAreaElement | null>(null)
   const displayRef = React.useRef<HTMLDivElement | null>(null)
-
-  const smallScreen = windowDimensions[0] < screens.sm
 
   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     writing.set(e.target.value).project()
@@ -79,7 +77,6 @@ function Editor() {
   }
   React.useEffect(() => {
     const handleWindowResize = () => {
-      setWindowDimensions([window.innerWidth, window.innerHeight])
       if (displayRef.current?.offsetHeight) {
         setDisplayHeight(displayRef.current?.offsetHeight - 20)
       }
@@ -101,53 +98,51 @@ function Editor() {
   }, [textMode])
 
   return (
-    <div className="flex flex-col items-center w-screen">
-      <div className="flex flex-col h-screen w-[344px] sm:w-[568px] p-[16px]">
-        <div>
-          <SettingsBar
-            settings={settings}
-            settingsValue={settingsValue}
-            textMode={textMode}
-            setTextMode={setTextMode}
-            copyWritingToClipbpoadAsync={copyWritingToClipbpoadAsync}
-            downloadSvgAsync={downloadSvgAsync}
-            smallScreen={smallScreen}
-          />
-        </div>
-        <div
-          className="w-[312px] sm:w-[536px] border rounded-lg p-2 my-2 flex-1 overflow-auto border-slate-700"
-          ref={displayRef}
-        >
-          <Display
-            writingSvg={writingSvg}
-            writingRep={writingRep}
-            cursorPosition={!textMode ? cursorPosition : null}
-            moveTo={cursor.moveTo}
-            glyphSize={settingsValue.scale * editorParameters.glyphBaseSize}
-            gridMode={gridMode}
-            lineWrap={settingsValue.lineWrap}
-            displayHeight={displayHeight}
-          />
-        </div>
-
-        <div className={!textMode ? 'hidden' : 'h-[216px]'}>
-          <textarea
-            ref={textareaRef}
-            value={writingValue}
-            onChange={handleTextChange}
-            className="border border-slate-700 rounded-lg p-2 h-[216px] w-[312px] sm:w-[536px]"
-          />
-        </div>
-        {!textMode && (
-          <div className="flex flex-row items-center">
-            <Keyboard
-              smallScreen={smallScreen}
-              writing={writing}
-              cursor={cursor}
-            />
-          </div>
-        )}
+    <div className="flex flex-col h-screen w-[344px] sm:w-[568px] p-[16px]">
+      <div>
+        <SettingsBar
+          settings={settings}
+          settingsValue={settingsValue}
+          textMode={textMode}
+          setTextMode={setTextMode}
+          copyWritingToClipbpoadAsync={copyWritingToClipbpoadAsync}
+          downloadSvgAsync={downloadSvgAsync}
+          smallScreen={smallScreen}
+        />
       </div>
+      <div
+        className="w-[312px] sm:w-[536px] border rounded-lg p-2 my-2 flex-1 overflow-auto border-slate-700"
+        ref={displayRef}
+      >
+        <Display
+          writingSvg={writingSvg}
+          writingRep={writingRep}
+          cursorPosition={!textMode ? cursorPosition : null}
+          moveTo={cursor.moveTo}
+          glyphSize={settingsValue.scale * editorParameters.glyphBaseSize}
+          gridMode={gridMode}
+          lineWrap={settingsValue.lineWrap}
+          displayHeight={displayHeight}
+        />
+      </div>
+
+      <div className={!textMode ? 'hidden' : 'h-[216px]'}>
+        <textarea
+          ref={textareaRef}
+          value={writingValue}
+          onChange={handleTextChange}
+          className="border border-slate-700 rounded-lg p-2 h-[216px] w-[312px] sm:w-[536px]"
+        />
+      </div>
+      {!textMode && (
+        <div className="flex flex-row items-center">
+          <Keyboard
+            smallScreen={smallScreen}
+            writing={writing}
+            cursor={cursor}
+          />
+        </div>
+      )}
     </div>
   )
 }
